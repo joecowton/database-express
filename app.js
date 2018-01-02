666467e337d6192f436d5ee568248850662e29b5
Joe Cowton <joecowton@gmail.com>
2018-01-02 19:53:50 +0000

    adds sequelize database

--- app.js
+++ app.js
@@ -3,8 +3,47 @@ const app = express()
 var session = require('express-session');
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('postgres://localhost:4000/mydb');
+const sequelize = new Sequelize('mydb', 'josephcowton', 'Thechronic1', {
+  host: 'localhost',
+  dialect: 'postgres',
+  pool: {
+          max: 5,
+          min: 0,
+          acquire: 30000,
+          idle: 10000
+        },
+});
 
+
+const User = sequelize.define('user', {
+  firstName: {
+    type: Sequelize.STRING
+  },
+  lastName: {
+    type: Sequelize.STRING
+  }
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+User.sync({force: true}).then(() => {
+  // Table created
+  return User.create({
+    firstName: 'John',
+    lastName: 'Hancock'
+  });
+});
+
+User.findOne().then(user => {
+  console.log(user.get('firstName'));
+});
 
 app.use(session({secret: "Shh, its a secret!"}));
 
